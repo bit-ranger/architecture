4a1b3552c91f74cfe8394e82fa6c666a7a9f53c9
dubuyuye <sincerebravefight@gmail.com>
2016-04-28 11:28:17 +0800

    重构

--- security/src/main/java/com/rainyalley/architecture/security/JitFilterInvocationSecurityMetadataSource.java
+++ security/src/main/java/com/rainyalley/architecture/security/JitFilterInvocationSecurityMetadataSource.java
@@ -1,4 +1,4 @@
-package com.rainyalley.architecture.web.user;
+package com.rainyalley.architecture.security;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
--- webapp-user/src/main/java/com/rainyalley/architecture/webapp/user/UserController.java
+++ webapp-user/src/main/java/com/rainyalley/architecture/webapp/user/UserController.java
@@ -1,11 +1,14 @@
-package com.rainyalley.architecture.web.user;
+package com.rainyalley.architecture.webapp.user;
 
+import com.rainyalley.architecture.common.user.model.entity.User;
 import com.rainyalley.architecture.common.user.service.UserService;
+import com.rainyalley.architecture.core.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -15,8 +18,8 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping
-    public String index(){
-        return "";
+    public List<User> index(){
+        return userService.get(new User(), new Page());
     }
 
     @RequestMapping("404")
