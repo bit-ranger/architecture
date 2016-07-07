30debaefcfdf41ac1e204ca2212e35428af6cedf
WakelessDragon <sincerebravefight@gmail.com>
2016-07-07 17:12:13 +0800

    fix rpc dependency conflict

--- rpc/src/test/java/com/rainyalley/architecture/rpc/provider/ProviderLauncherTest.java
+++ rpc/src/test/java/com/rainyalley/architecture/rpc/provider/ProviderLauncherTest.java
@@ -1,19 +1,15 @@
 package com.rainyalley.architecture.rpc.provider; 
 
-import org.junit.Test; 
-import org.junit.Before; 
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
 /** 
 * ProviderLauncher Tester. 
-*/ 
-@RunWith(SpringJUnit4ClassRunner.class) 
-@ContextConfiguration(locations = "classpath:spring-rpc-provider.xml")
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+*/
+@RunWith(BlockJUnit4ClassRunner.class)
 public class ProviderLauncherTest { 
 
     @Before
