fa9c4cba77470756191d496dc422f5cb3c43a358
WakelessDragon <sincerebravefight@gmail.com>
2017-11-17 16:29:45 +0800

    logging aspect

--- service/src/main/java/com/rainyalley/architecture/aop/PointContextCollectAspect.java
+++ service/src/main/java/com/rainyalley/architecture/aop/PointContextCollectAspect.java
@@ -3,16 +3,14 @@ package com.rainyalley.architecture.aop;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-@Aspect
+//@Component
+//@Aspect
 public class PointContextCollectAspect implements PointContextProvider {
 
     private static final ThreadLocal<List<PointContext>> concurrentJoinPoints = new ThreadLocal<List<PointContext>>();
