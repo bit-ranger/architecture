a94465d9ea86eda7b37f603c9220ea3cf73c868e
WakelessDragon <sincerebravefight@gmail.com>
2017-11-17 15:54:04 +0800

    code refactoring

--- boot/src/main/java/com/rainyalley/architecture/boot/BootApplication.java
+++ boot/src/main/java/com/rainyalley/architecture/boot/BootApplication.java
@@ -3,7 +3,7 @@ package com.rainyalley.architecture.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.rainyalley.architecture.boot","com.rainyalley.architecture.service"})
+@SpringBootApplication(scanBasePackages = {"com.rainyalley.architecture"})
 public class BootApplication {
 
 	public static void main(String[] args) {
