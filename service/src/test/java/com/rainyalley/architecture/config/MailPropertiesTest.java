64a8f791b854ae1f6d6dd4952ca5c64cb303b830
WakelessDragon <sincerebravefight@gmail.com>
2017-11-21 14:45:53 +0800

    lombok and configurationProperties

--- service/src/test/java/com/rainyalley/architecture/config/MailPropertiesTest.java
+++ service/src/test/java/com/rainyalley/architecture/config/MailPropertiesTest.java
@@ -3,6 +3,7 @@ package com.rainyalley.architecture.config;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,10 +18,12 @@ public class MailPropertiesTest {
     @Resource
     private MailProperties mailProperties;
 
-
+    @Value("${mail.host}")
+    private String location;
 
     @Test
     public void run(){
-        Assert.assertEquals(mailProperties.getHost(),"localhost");
+        Assert.assertNotNull(location);
+        Assert.assertEquals(mailProperties.getHost(),location);
     }
 }
