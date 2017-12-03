62aae700d802591fcd94e043431b6449a03855cd
dubuyuye <mm331320ZB>
2017-12-03 20:14:51 +0800

    dependency

--- service/src/main/java/com/rainyalley/architecture/config/MailProperties.java
+++ service/src/main/java/com/rainyalley/architecture/config/MailProperties.java
@@ -4,12 +4,14 @@ import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "mail")
+@Validated
 public class MailProperties {
 
     public static class Smtp {
