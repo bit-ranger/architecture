bbe3a2e52ae008efecb861fd5fa26c0ef3128621
WakelessDragon <sincerebravefight@gmail.com>
2018-01-17 13:40:19 +0800

    DoubleWayRead MergeSort

--- core/src/test/java/com/rainyalley/architecture/core/arithmetic/sort/DoubleWayReadStoreMergeSortTest.java
+++ core/src/test/java/com/rainyalley/architecture/core/arithmetic/sort/DoubleWayReadStoreMergeSortTest.java
@@ -27,7 +27,7 @@ public class DoubleWayReadStoreMergeSortTest {
         File file = new File("/var/sort/architecture_user.csv");
         file.getParentFile().mkdirs();
         BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 100000; i++) {
             long id = radom.nextLong();
             bw.write( id + ",中文名字A,中文密码,1");
             bw.newLine();
