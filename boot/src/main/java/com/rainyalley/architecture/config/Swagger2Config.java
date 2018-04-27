efe5033e5800919a855ff54eacb7d30e42881012
WakelessDragon <sincerebravefight@gmail.com>
2018-04-27 14:56:56 +0800

    修正配置错误

--- boot/src/main/java/com/rainyalley/architecture/config/Swagger2Config.java
+++ boot/src/main/java/com/rainyalley/architecture/config/Swagger2Config.java
@@ -20,7 +20,7 @@ public class Swagger2Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.rainyalley.architecture.boot.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.rainyalley.architecture.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
