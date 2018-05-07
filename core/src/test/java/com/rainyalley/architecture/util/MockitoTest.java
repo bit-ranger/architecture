688c84785d08884d8642390b55bd609e4543ba66
WakelessDragon <sincerebravefight@gmail.com>
2018-05-07 21:08:52 +0800

    mockito

--- core/src/test/java/com/rainyalley/architecture/util/MockitoTest.java
+++ core/src/test/java/com/rainyalley/architecture/util/MockitoTest.java
@@ -4,11 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MockitoTest {
