e7d6c24c5e25f664d18c9f43ebdefc24e84ccfb1
WakelessDragon <sincerebrave@gmail.com>
2018-08-16 12:19:01 +0800

    remove agent

--- agent/src/main/java/com/rainyalley/architecture/agent/runtime/TimeTracking.java
+++
@@ -1,8 +0,0 @@
-package com.rainyalley.architecture.agent.runtime;
-
-public class TimeTracking {
-
-    public static void track(String methodName, long startNanoTime, long endNanoTime){
-        System.out.println(String.format("trcking %s %s %s", methodName, startNanoTime, endNanoTime));
-    }
-}
--- agent/src/test/java/com/rainyalley/architecture/test/AgentTest.java
+++
@@ -1,9 +0,0 @@
-package com.rainyalley.architecture.test;
-
-
-public class AgentTest {
-
-    public static void main(String[] args){
-        System.out.println("hello world");
-    }
-}
