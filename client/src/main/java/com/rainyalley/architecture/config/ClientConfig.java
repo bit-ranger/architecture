c620d7543af70df9f696bcd85b278f743abe002c
WakelessDragon <sincerebrave@gmail.com>
2018-09-11 14:13:12 +0800

    zuul router

--- client/src/main/java/com/rainyalley/architecture/config/ClientConfig.java
+++ client/src/main/java/com/rainyalley/architecture/config/ClientConfig.java
@@ -3,10 +3,12 @@ package com.rainyalley.architecture.config;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@EnableZuulProxy
 @EnableHystrix
 @EnableDiscoveryClient
 @Configuration
