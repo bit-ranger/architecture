8d12c1ffe3bcca1e88e1d9e524ae955e88d3779b
WakelessDragon <sincerebrave@gmail.com>
2019-02-13 20:54:08 +0800

    merge from master

--- web/src/main/java/com/rainyalley/architecture/WebApplication.java
+++ web/src/main/java/com/rainyalley/architecture/WebApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(
 	scanBasePackages = {
-		"com.rainyalley.architecture.graphql",
 		"com.rainyalley.architecture.config",
 		"com.rainyalley.architecture.controller",
 		"com.rainyalley.architecture.impl"})
